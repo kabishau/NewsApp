33cc86f3606ab770f129033ba57e4c5619602405
user139405 <user139405@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2018-10-15 13:27:58 -0400

    populating table view with some news

--- NewsApp/NewsTableViewController.swift
+++ NewsApp/NewsTableViewController.swift
@@ -1,6 +1,24 @@
 import UIKit
 
 class NewsTableViewController: UITableViewController {
+    
+    var titles:[String] = [
+        "New York Lakers Score Again!",
+        "Apple Presents New iWatch",
+        "Deeplink.me Wants To Break Open App Discovery",
+        "Silly Cat Attempts Jump And Hits Air",
+        "New MacBook Air So Thin People Can't See It",
+        "Higgs-Boson Finally Discovered In Scientists Coat"
+    ]
+    
+    var authors:[String] = [
+        "Bob",
+        "Alice",
+        "Reinder",
+        "Aleksey",
+        "Ford",
+        "Zaphod"
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -11,12 +29,22 @@ class NewsTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
 
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 
-        return 0
+        return titles.count
     }
-
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell: UITableViewCell? = tableView.dequeueReusableCell(withIdentifier: "cellIndetifier")
+        if cell == nil {
+            cell = UITableViewCell(style: UITableViewCell.CellStyle.subtitle, reuseIdentifier: "cellIndetifier")
+        }
+        cell!.textLabel?.text = titles[indexPath.row]
+        cell!.detailTextLabel?.text = authors[indexPath.row]
+        
+        return cell!
+    }
 }
