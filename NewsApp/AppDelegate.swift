f1ea8c00c1b4dd18ed90278976895ab2a6aadb2e
user139405 <user139405@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2018-10-15 12:11:00 -0400

    setting initial load of navigation controller and table view controller

--- NewsApp/AppDelegate.swift
+++ NewsApp/AppDelegate.swift
@@ -5,9 +5,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        let frame = UIScreen.main.bounds
+        window = UIWindow(frame: frame)
+        
+        let newsTableVC = NewsTableViewController()
+        newsTableVC.title = "News"
+        let navigationVC = UINavigationController(rootViewController: newsTableVC)
+        
+        window?.rootViewController = navigationVC
+        window?.makeKeyAndVisible()
+
         return true
     }
 
