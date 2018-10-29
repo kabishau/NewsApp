8d996ab703805c1b3de644277ff1fc0bfcde7db8
user139405 <user139405@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2018-10-29 16:58:25 -0400

    API class

--- NewsApp/API/API.swift
+++ NewsApp/API/API.swift
@@ -1,3 +1,15 @@
 import Foundation
 import Alamofire
 import SwiftyJSON
+
+// global variable, it's outside the class
+// private makes it visible and accessible within current file
+private let _API_SharedInstance = API()
+
+class API {
+    // computed class property
+    // class property returns private global variable
+    class var sharedInstance: API {
+        return _API_SharedInstance
+    }
+}
