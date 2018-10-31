1890464657fbfe6bb30d3614c8780803651d0818
AlekseyK <aleksey.kabishau@gmail.com>
2018-10-30 20:52:35 -0400

    adding article model

---
+++ NewsApp/API/Article.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+class Article {
+    
+    var id: Int = 0
+    var title: String = ""
+    var author: String = ""
+    var content: String = ""
+    var thumbnailURL: String = ""
+    var articleURL: String = ""
+    var excerpt: String = ""
+    var creationDate: Date = Date()
+}
